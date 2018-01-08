8d395437c21e7b75fc9e6aac158c6ba4a22c4c67
kba239 <adams.kate.b@gmail.com>
2018-01-08 14:17:55 -0800

    SEP Databases 03 Complete

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -1,26 +1,179 @@
 require 'sqlite3'
 
 module Selection
-  def find(id)
-    row = connection.get_first_row <<-SQL
-      SELECT #{columns.join ","} FROM #{table}
-      WHERE id = #{id};
-    SQL
 
-    data = Hash[columns.zip(row)]
-    new(data)
+  def find(*ids)
+    if ids.length == 1
+      find_one(ids.first)
+    else
+      ids.each do |id|
+        if id.is_a?(Integer) && id > 0
+          next
+        else
+          puts "One or more of these values is not a valid ID."
+          return -1
+        end
+      end
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id IN (#{ids.join(",")});
+      SQL
+
+      rows_to_array(rows)
+    end
+  end
+
+  def find_one(id)
+    if id.is_a?(Integer) && id > 0
+      row = connection.get_first_row <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id = #{id};
+      SQL
+      init_object_from_row(row)
+    else
+      puts "That ID is not valid."
+      return -1
+    end
   end
 
   def find_by(attribute, value)
-    value = BlocRecord::Utility.sql_strings(value)
-    rows = connection.execute <<-SQL
+    row = connection.get_first_row <<-SQL
       SELECT #{columns.join ","} FROM #{table}
-      WHERE #{attribute} = #{value};
+      WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
     SQL
 
-    rows.map do |row|
+    init_object_from_row(row)
+  end
+
+  def take(num=1)
+    if num > 1
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        ORDER BY random()
+        LIMIT #{num};
+      SQL
+
+      rows_to_array(rows)
+    else
+      take_one
+    end
+  end
+
+  def take_one
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY random()
+      LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end
+
+  def first
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY id ASC LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end
+
+  def last
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY id DESC LIMIT 1;
+    SQL
+
+    init_object_from_row(row)
+  end
+
+  def all
+    rows = connection.execute <<-SQL
+      SELECT #{columns.join ","} FROM #{table};
+    SQL
+
+    rows_to_array(rows)
+  end
+
+  def find_each(options = {})
+    start = options[:start]
+    batch_size = options[:batch_size]
+    if start != nil && batch_size != nil
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        LIMIT #{batch_size} OFFSET #{start};
+      SQL
+    elsif start == nil && batch_size != nil
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        LIMIT #{batch_size};
+      SQL
+    elsif start != nil && batch_size == nil
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        OFFSET #{start};
+      SQL
+    else
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table};
+      SQL
+    end
+
+    rows.each do |row|
+      yield init_object_from_row(row)
+    end
+  end
+
+  def find_in_batches(options = {})
+    start = options[:start]
+    batch_size = options[:batch_size]
+    if start != nil && batch_size != nil
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        LIMIT #{batch_size} OFFSET #{start};
+      SQL
+    elsif start == nil && batch_size != nil
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        LIMIT #{batch_size};
+      SQL
+    elsif start != nil && batch_size == nil
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        OFFSET #{start};
+      SQL
+    else
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table};
+      SQL
+    end
+
+    row_array = rows_to_array(rows)
+    yield(row_array)
+  end
+
+  private
+  def init_object_from_row(row)
+    if row
       data = Hash[columns.zip(row)]
       new(data)
+    end
+  end
+
+  def rows_to_array(rows)
+    rows.map { |row| new(Hash[columns.zip(row)]) }
+  end
+
+  def method_missing(method_name, *args)
+    if method_name.match(/find_by_/)
+      attribute = method_name.to_s.split('find_by_')[1]
+      if columns.include?(attribute)
+        find_by(attribute, *args)
+      else
+        puts "#{attribute} does not exist in the database."
+      end
+    else
+      super
     end
   end
 end
