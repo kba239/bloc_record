b2bad7c84588d9e694aecd24b4372b8f24cd8a8e
kba239 <adams.kate.b@gmail.com>
2018-01-11 10:54:28 -0800

    SEP Databases 4 Complete

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -152,7 +152,89 @@ module Selection
     yield(row_array)
   end
 
+  def where(*args)
+    if args.count > 1
+      expression = args.shift
+      params = args
+    else
+      case args.first
+      when String
+        expression = args.first
+      when Symbol
+        expression = args.first.to_s
+      when Hash
+        expression_hash = BlocRecord::Utility.convert_keys(args.first)
+        expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+      end
+    end
+
+    sql = <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      WHERE #{expression};
+    SQL
+
+    rows = connection.execute(sql, params)
+    rows_to_array(rows)
+  end
+
+
+  def order(*args)
+    orderedArray = []
+    args.each do |arg|
+      case arg
+      when String
+        orderedArray << arg
+      when Symbol
+        orderedArray << arg.to_s
+      when Hash
+        orderedArray << arg.map{|key, value| "#{key} #{value}"}
+      end
+    end
+
+    order = orderedArray.join(",")
+    puts order
+
+    rows = connection.execute <<-SQL
+      SELECT * FROM #{table}
+      ORDER BY #{order};
+    SQL
+    rows_to_array(rows)
+  end
+
+  def join(*args)
+    if args.count > 1
+      joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id"}.join(" ")
+      rows = connection.execute <<-SQL
+        SELECT * FROM #{table} #{joins}
+      SQL
+    else
+      case args.first
+      when String
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+        SQL
+      when Symbol
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table}
+          INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
+        SQL
+      when Hash
+        key = args.first.keys.first
+        value = args.first[key]
+        puts "#{key}, #{value}"
+        rows = connection.execute <<-SQL
+          SELECT * FROM #{table}
+          INNER JOIN #{key} ON #{key}.#{table}_id = #{table}.id
+          INNER JOIN #{value} ON #{value}.#{key}_id = #{key}.id
+        SQL
+      end
+    end
+
+    rows_to_array(rows)
+  end
+
   private
+
   def init_object_from_row(row)
     if row
       data = Hash[columns.zip(row)]
