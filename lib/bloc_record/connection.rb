490f1242990e2fd379461553ea4bd9c1b9cbc607
kba239 <adams.kate.b@gmail.com>
2018-01-18 16:56:08 -0800

    SEP Databases 7 Complete

--- lib/bloc_record/connection.rb
+++ lib/bloc_record/connection.rb
@@ -1,7 +1,9 @@
 require 'sqlite3'
+require 'pg'
 
  module Connection
   def connection
-    @connection ||= SQLite3::Database.new(BlocRecord.database_filename)
+    BlocRecord.database == :sqlite3 ? @connection ||= SQLite3::Database.new(BlocRecord.database_filename)
+    BlocRecord.database == :pg ? @connection ||= PG.connect(dbname: BlocRecord.database_filename)
   end
 end
